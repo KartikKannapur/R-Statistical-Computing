9e6f8b0648d985d04054fde4c5b7d121ad485c63
Kartik Kannapur <kartikkannapur@gmail.com>
2016-02-22 21:18:27 +0530

    Learning R - Chapter 02 - Complete

---
+++ Learning_R/Chapter_02.R
@@ -0,0 +1,42 @@
+########################
+# # --- Chapter 02 --- #
+########################
+
+1 + 1
+
+a <- 1:5
+b <- 11:15
+a + b
+
+# #Creating a sequence
+1:10
+
+# #Concatenating a sequence to create a vector
+c(1:10)
+
+# #Subtract a constant from a vector - subtracts it from each value in the vector
+c <- c(11:15)
+c - 2
+
+
+factorial(5)
+
+# #Boolean
+c(3, 4-1, 1+1+1, 2+1, 4) == 3
+
+
+"
+R has the concept of Dynamic Typing. It infers the datatype of the variable by 
+looking at the value assigned to it.
+"
+
+"
+Global Assignment <<-
+"
+
+
+# #Representing Infinity
+Inf
+
+
+# #Logical Values - True, False, NA
